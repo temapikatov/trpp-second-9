e7d42e95687b80dcc57214d7a5d2afdabae9df2a
temapikatov <temapikatov@gmail.com>
2022-04-01 17:22:42 +0300

    da

--- src/main/java/ru/mirea/trpp_second_9/controllers/HealthController.java
+++ src/main/java/ru/mirea/trpp_second_9/controllers/HealthController.java
@@ -3,6 +3,7 @@ package ru.mirea.trpp_second_9.controllers;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import ru.mirea.trpp_second_9.entity.HealthResponse;
 
 /** Проверка состояния сервера. */
 @Controller()
